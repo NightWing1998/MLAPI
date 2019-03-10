97254d8be98654ebbdb436d2db0d021e15675b95
NightWing1998 <dhruvil.s@somaiya.edu>
2019-03-11 00:20:52 +0530

    generation of random w and b

--- neural-networks/index.js
+++ neural-networks/index.js
@@ -19,14 +19,13 @@ module.exports = function(path,hidden_layers,no_of_nodes,learningRate){		//comme
 		for(let a = 0; a < data.length - 1;a++){
 			// console.log(data[a]);
 			x[a] = [];
-			x[a][0] = 1;
 			// data[a] = data[a].slice(1,data[a].length-2);
 			data[a] = data[a].split(",");
 			for(let b = 0;b < data[a].length - 1 ;b++){
-				x[a][b+1] = parseFloat(data[a][b]);
+				x[a][b] = parseFloat(data[a][b]);
 			}
 			// console.log(x[a]);
-			y[a] = parseFloat(data[a][data[a].length-1]);
+			y[a] = [parseFloat(data[a][data[a].length-1])];
 	
 		}
 		
@@ -34,32 +33,41 @@ module.exports = function(path,hidden_layers,no_of_nodes,learningRate){		//comme
 		
 		var omega = [],b = [];
 		for(var i = 0;i<=hl;i++){
-			var limit = 0;
+			var limit = 0,cur_nodes = 0;
 			omega[i] = [];
 			b[i] = [];
 			if(i === hl){
 				// omega[hl] sould have elements of upto y.length
-				limit = y.length;
+				limit = y[0].length;
 			}else{
 				limit = nodes;
 			}
 			// limit = (i === hl)?y.length:nodes;
 			for(let j = 0;j < limit;j++){
 				omega[i][j] = [];
-				b[i][j] = [];
+				b[i][j] = [1];
+				if( i === 0 ){
+					cur_nodes = x[0].length;
+				} else{
+					cur_nodes = nodes;
+				}
+				for(let k = 0;k < cur_nodes;k++){
+					omega[i][j][k] = [Math.random()];
+				}
 			}
 		}
 		// theta[a][0] = Math.random();
 
-		console.log("neural ",x,y,theta );	//comment for deployemnt
+		console.log("neural\n>>>>>>>>>\n",x,"\n<<<<<<<<<<<\n",y,"\n#############\n",JSON.stringify(omega),"\n???????????\n",b );	//comment for deployemnt
 		
+		console.log("omega lengths: ",omega.length,omega[0].length,omega[0][0].length);
 		// console.log("Theta:- ",theta);
 	
 		// theta = backpropogation(x, y, theta, learningRate);			//comment this out for testing
 		// theta = backpropogation(x,y,theta,0.05)						//comment this out for deployment
-		console.log("Optimal value of theta for current input:- ",theta);
+		// console.log("Optimal value of theta for current input:- ",omega,b);
 		
-		callback(theta);		//comment this out for testing
+		// callback(omega,b);		//comment this out for testing
 	
 	});
 };
