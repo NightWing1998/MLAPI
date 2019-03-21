01d6da4d9c4cc43b0c257f916419be418bb05668
NightWing1998 <dhruvil.s@somaiya.edu>
2019-03-22 01:03:40 +0530

    completion of backpropogation

--- neural-networks/index.js
+++ neural-networks/index.js
@@ -1,77 +1,81 @@
-var fs 				= require('fs')
-	backpropogation = require('./backpropogation');
-	// costCalculation = require('./cost-calculation');
+var fs = require('fs')
+backpropogation = require('./backpropogation');
+cost = require('./cost-calculation');
 
-module.exports = function(path,hidden_layers,no_of_nodes,learningRate){		//comment this out for testing
+module.exports = function (path, hidden_layers, no_of_nodes, activationFunction, deriActivationFunction, learningRate, callback) {		//comment this out for testing
 	// console.log(path);
-	var hl = hidden_layers,nodes=no_of_nodes;
-	fs.readFile('data.txt',function (err,data) {		//comment for deployment
-	// fs.readFile(path,function (err,data) {			//comment this out for testing
-		// console.log("Reading input from ",path);		//comment this out for testing
-		if(err) throw err;	
+	var hl = hidden_layers, nodes = no_of_nodes;
+	// fs.readFile('data.txt', function (err, data) {		//comment for deployment
+	fs.readFile(path, function (err, data) {				//comment this out for testing
+		console.log("Reading input from ",path);			//comment this out for testing
+		if (err) throw err;
 		// console.log(data.toString());	
 		data = data.toString();
 		// data = data.slice(1,data.length);
 		// console.log(data);								//comment for deployment
-		var x = [],y = [];
+		var x = [], y = [];
 		// console.log( data.split('\n') );
 		data = data.split('\n');
-		for(let a = 0; a < data.length - 1;a++){
+		for (let a = 0; a < data.length - 1; a++) {
 			// console.log(data[a]);
 			x[a] = [];
 			// data[a] = data[a].slice(1,data[a].length-2);
 			data[a] = data[a].split(",");
-			for(let b = 0;b < data[a].length - 1 ;b++){
+			for (let b = 0; b < data[a].length - 1; b++) {
 				x[a][b] = parseFloat(data[a][b]);
 			}
 			// console.log(x[a]);
-			y[a] = [parseFloat(data[a][data[a].length-1])];
-	
+			y[a] = [parseFloat(data[a][data[a].length - 1])];
+
 		}
-		
+
 		// console.log("neural ",x[x.length-1],y[y.length-1],x.length,x[0].length,y.length );	//comment for deployemnt
-		
-		var omega = [],b = [];
-		for(var i = 0;i<=hl;i++){
-			var limit = 0,cur_nodes = 0;
+
+		var omega = [], b = [];
+		for (var i = 0; i <= hl; i++) {
+			var limit = 0, cur_nodes = 0;
 			omega[i] = [];
 			b[i] = [];
-			if(i === hl){
+			if (i === hl) {
 				// omega[hl] sould have elements of upto y.length
 				limit = y[0].length;
-			}else{
+			} else {
 				limit = nodes;
 			}
 			// limit = (i === hl)?y.length:nodes;
-			for(let j = 0;j < limit;j++){
+			for (let j = 0; j < limit; j++) {
 				omega[i][j] = [];
-				b[i][j] = 1;
-				if( i === 0 ){
+				b[i][j] = 0;
+				if (i === 0) {
 					cur_nodes = x[0].length;
-				} else{
+				} else {
 					cur_nodes = nodes;
 				}
-				for(let k = 0;k < cur_nodes;k++){
-					omega[i][j][k] = Math.random();
+				for (let k = 0; k < cur_nodes; k++) {
+					// omega[i][j][k] = Math.random();
+					omega[i][j][k] = 0;
 				}
 			}
 		}
 		// theta[a][0] = Math.random();
 
-		// console.log("neural\n>>>>>>>>>\n",x,"\n<<<<<<<<<<<\n",y,"\n#############\n",omega,"\n???????????\n",b );	//comment for deployemnt
-		
+		// console.log("neural\n>>>>>>>>>\n", x, "\n<<<<<<<<<<<\n", y, "\n#############\n", omega, "\n???????????\n", b);	//comment for deployemnt
+
 		// console.log("omega lengths: ",omega.length,omega[0].length,omega[0][0].length);
 		// console.log("Theta:- ",theta);
 
-		omega = backpropogation(x,y,hl,omega,b,ReLU,deReLU,learningRate);	//comment this during testing
-		omega = backpropogation(x,y,hl,omega,b,ReLU,deReLU,0.05);	//comment this during testing
+		omega = backpropogation(x, y, hl, omega, b, activationFunction, deriActivationFunction, learningRate);	//comment this during testing
+		// omega = backpropogation(x,y,hl,omega,b,ReLU,deReLU,0.05);	//comment this during deployment
 
 		b = omega.b;
 		omega = omega.omega;
 
-		// console.log("Optimal value of theta for current input:- ",omega,b);
-		
-		// callback(omega,b);		//comment this out for testing
-	
+		// console.log("Optimal value of omega b for current input:- ", omega, "\t", b);
+		// var hypo = require("./hypothesis");
+		// let h = hypo([[74.77589300092767, 89.52981289513276]], 0,omega,b,activationFunction).a[1];
+		// console.log("*********",h);
+
+		callback(omega, b);		//comment this out for testing
+
 	});
 };
--- neural-networks/test.js
+++ neural-networks/test.js
@@ -1,17 +1,22 @@
-var x = [[1,1,1,1],[2,2,2,2],[1,1,1,1],[2,2,2,2]];
-var w = [ [ [ 1,1,1,1] , [2,2,2,2] , [3,3,3,2] ],[  [1,0,0] , [0,1,0] , [0,0,1]  ],[  [1,1,1] , [1,1,-1] ] ]
-var b = [ [ 0,0,0 ], [0,0,0] , [0,0]  ]
-var hypo = require("./hypothesis");
-var cost = require("./cost-calculation");
-var y = [ [ 0.9999999999622486, 0.5 ],
-[ 1, 0.5 ],
-[ 0.9999999999622486, 0.5 ],
-[ 1, 0.5 ] ];
-ReLU        = require("./activationFunctions").ReLU,
+// var x = [[1,1,1,1],[2,2,2,2],[1,1,1,1],[2,2,2,2]];
+// var w = [ [ [ 1,1,1,1] , [2,2,2,2] , [3,3,3,2] ],[  [1,0,0] , [0,1,0] , [0,0,1]  ],[  [1,1,1] , [1,1,-1] ] ]
+// var b = [ [ 0,0,0 ], [0,0,0] , [0,0]  ]
+// var hypo = require("./hypothesis");
+// var cost = require("./cost-calculation");
+// var y = [ [ 0.9999999999622486, 0.5 ],
+// [ 1, 0.5 ],
+// [ 0.9999999999622486, 0.5 ],
+// [ 1, 0.5 ] ];
+var ReLU        = require("./activationFunctions").ReLU,
 deriReLU    = require("./derivativeOfactivation").ReLU;
-var backpropogation = require("./backpropogation");
-// console.log(hypo(x,2,w,b,ReLU).a[3]);
-backpropogation(x,y,2,w,b,ReLU,deriReLU,0.005);
-console.log("w:",w,"\n\n\n b: ",b);
-var h = hypo(x,2,w,b,ReLU).a[3];
-console.log("@@@@@@@@@@@@@@@@",cost(y,h));
+// var backpropogation = require("./backpropogation");
+// // console.log(hypo(x,2,w,b,ReLU).a[3]);
+// backpropogation(x,y,2,w,b,ReLU,deriReLU,0.00005);
+// console.log("w:",w,"\n\n\n b: ",b);
+// var h = hypo(x,2,w,b,ReLU).a[3];
+// console.log("@@@@@@@@@@@@@@@@",cost(y,h));
+
+var neural = require("./index");
+neural("./data.txt",0,2,ReLU,deriReLU,0.005,function(omega,b){
+	console.log(omega,b);
+});
